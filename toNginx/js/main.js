c4337c46285e943ea8ced20cf60db4adc8b36eb9
Orbweaver <coltonclemmerdev@gmail.com>
2015-10-02 11:13:12 -0400

    Font and favicon change

--- toNginx/js/main.js
+++ toNginx/js/main.js
@@ -7,7 +7,7 @@ var port = "81";
 
 
 function heartbeat(){
-	$.get(address+port+"/h/eartbeat", function(data){
+	$.get(address+":"+port+"/h/eartbeat", function(data){
 		if(!data)
 			console.log("Something went wrong");
 		setTimeout(heartbeat, 5000);
@@ -29,7 +29,7 @@ $(document).ready(function(){
 	}
 	if(hash=="about")
 		$("#blog").show();
-	$.get(address+port+"/"+hash, function(data){
+	$.get(address+":"+port+"/"+hash, function(data){
 			$(".info").append(data);
 			$(".info").fadeIn('fast');
 		});
@@ -49,7 +49,7 @@ $(document).ready(function(){
 	});
 	$('.home').click(function(){
 		window.location.hash = "about";
-		$.get(address+port+"/about", function(data){
+		$.get(address+":"+port+"/about", function(data){
 			$(".info").hide();
 			$(".info").empty();
 			$(".info").append(data);
@@ -59,7 +59,7 @@ $(document).ready(function(){
 	$('.contact').click(function(e){
 		e.preventDefault();
 		window.location.hash = "contact";
-		$.get(address+port+"/contact", function(data){
+		$.get(address+":"+port+"/contact", function(data){
 			$(".info").hide();
 			$(".info").empty();
 			$(".info").append(data);
@@ -70,7 +70,7 @@ $(document).ready(function(){
 	$('.blog').click(function(e){
 		e.preventDefault();
 		window.location.hash = "blog";
-		$.get(address+port+"/blog", function(data){
+		$.get(address+":"+port+"/blog", function(data){
 			$(".info").hide();
 			$(".info").empty();
 			$(".info").append(data);
@@ -83,7 +83,7 @@ $(document).ready(function(){
 		if(window.innerWidth<=1200){
 			hover = $(this).attr('id');
 			hover = hover.toLowerCase();
-			$.get((address+port+"/"+hover), function(data){
+			$.get((address+":"+port+"/"+hover), function(data){
 				info = data;
 				$(".info").hide();
 				$(".info").empty();
@@ -101,7 +101,7 @@ $(document).ready(function(){
 		$(this).animate({opacity: '1'});
 		hover = $(this).attr('id');
 		hover = hover.toLowerCase();
-		$.get((address+port+"/"+hover), function(data){
+		$.get((address+":"+port+"/"+hover), function(data){
 			info = data;
 		});
 	});
